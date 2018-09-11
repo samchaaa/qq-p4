c60b9163c04d89475bb9056b8074f5c5ab7f319e
Sam Chakerian <samchakerian+git@gmail.com>
2018-09-10 19:14:17 -0700

    Add files via upload

---
+++ importer3.py
@@ -0,0 +1,13 @@
+from sklearn.externals import joblib
+
+class ImporterClass(self):
+
+    def __init__(self):
+
+        pass
+
+    def ImporterFunction(self):
+
+        asdf = joblib.load('./model_AGG.sav')
+
+        return asdf
