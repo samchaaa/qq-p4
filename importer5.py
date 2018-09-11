b3a5b6059370882a23273b27e11dc880326d5cf4
Sam Chakerian <samchakerian+git@gmail.com>
2018-09-10 19:25:02 -0700

    Add files via upload

--- importer5.py
+++ importer5.py
@@ -1,3 +1,5 @@
 from sklearn.externals import joblib
 
-asdf = joblib.load('./model_AGG.sav')
+AGG = joblib.load('./model_AGG.sav')
+AGM = joblib.load('./model_AGM.sav')
+AQM = joblib.load('./model_AQM.sav')
